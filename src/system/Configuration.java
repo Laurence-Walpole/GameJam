1913b8436c69f86d34e827a614cd64ebf03a19e5
Laurence-Walpole <me@laurence-walpole.me>
2018-01-26 19:13:33 +0000

    Window Created with a basic structure

---
+++ src/system/Configuration.java
@@ -0,0 +1,20 @@
+package system;
+
+public class Configuration {
+
+    private static String gameName = "Fuck Knows Right Now";
+    private static int gameSizeX = 1280, gameSizeY = 720;
+
+
+    public static String getGameName() {
+        return gameName;
+    }
+
+    public static int getGameSizeX() {
+        return gameSizeX;
+    }
+
+    public static int getGameSizeY() {
+        return gameSizeY;
+    }
+}
